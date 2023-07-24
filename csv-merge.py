803b6fd81c15d10c4560bd63892a3ce706fcfb77
Ambrus Kuszal <a84282967@china.huawei.com>
2023-07-24 14:04:39 +0200

    p install atlassian-python-api pip install atlassian-python-api

--- csv-merge.py
+++ csv-merge.py
@@ -1,18 +1,17 @@
 import pandas as pd
 
 csv1 = pd.read_csv("book2.csv")
-#csv1.head()
 csv2 = pd.read_csv("book1.csv")
-#csv2.head()
 
-# using merge function by setting how='inner'
+#Using merge function by setting how='inner'
 output1 = pd.merge(csv1, csv2, 
                    on='name', 
                    how='left')
-  
-output1.to_csv('data_merge.csv', index = False)  # Export merged pandas DataFrame
 
-# displaying result
+# Export merged pandas DataFrame s
+output1.to_csv('data_merge.csv', index = False)  
+
+#Displaying result
 print(output1)
 
 
